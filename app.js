f37cd13c358202f221ec0fdbb3140143b2329f7a
Thomas Fanella <fanella@purdue.edu>
2016-04-24 19:55:41 -0400

    Added facebook login

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ var express = require('express');
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
 
+// account passport authentication
+var passport = require('passport');
+
+// facebook strategy for passport
+var FacebookStrategy = require('passport-facebook').Strategy;
+
 // create a new express server
 var app = express();
 
@@ -26,4 +32,40 @@ app.listen(appEnv.port, '0.0.0.0', function() {
 
 	// print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
+});
+
+var FACEBOOK_APP_ID = 265118990493490;
+var FACEBOOK_APP_SECRET = "21acbc29fac79d69188785a6f36b3b6c";
+var profile;
+
+passport.use(new FacebookStrategy({
+    clientID: FACEBOOK_APP_ID,
+    clientSecret: FACEBOOK_APP_SECRET,
+    callbackURL: "http://fightingbooleans.mybluemix.net/"
+    //callbackURL: "http://www.example.com/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    User.findOrCreate(function(err, user) {
+      if (err) { return done(err); }
+      done(null, user);
+    });
+  }
+));
+
+// API endpoints
+// Redirect the user to Facebook for authentication.  When complete,
+// Facebook will redirect the user back to the application at
+//     /auth/facebook/callback
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+// Facebook will redirect the user to this URL after approval.  Finish the
+// authentication process by attempting to obtain an access token.  If
+// access was granted, the user will be logged in.  Otherwise,
+// authentication has failed.
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { successRedirect: '/',
+                                      failureRedirect: '/login' }));
+
+app.get('/print', function () {
+  console.log(profile);
 });
