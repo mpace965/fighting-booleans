9461401d353196134ae1fe323fd14365bfa51090
Matthew Pace <pace4@purdue.edu>
2016-04-24 20:40:53 -0400

    Add react-router

--- views/mainView.jsx
+++ views/mainView.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import LoginView from './loginView';
 import Header from './components/header';
@@ -7,37 +8,23 @@ import Header from './components/header';
 class Wrapper extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      activeView: LoginView,
-      activeViewState: {}
-    }
-  }
-
-  /* Handles navigation from all child views.
-  Optionally, you can pass in a state that will get applied to the activeView.
-  This is not automatic, you need to apply it in the view's componentWillMount.*/
-  setActiveView(activeView, state) {
-    this.setState({activeView: activeView});
-
-    if (state != null) {
-      this.setState({activeViewState: state});
-    }
   }
 
   render() {
-    var ActiveView = this.state.activeView;
 
     return (
       <div>
         <Header />
-        <ActiveView setActiveView={this.setActiveView} activeViewState={this.state.activeViewState} />
+        {this.props.children}
       </div>
     );
   }
 }
 
-ReactDOM.render(
-  <Wrapper />,
-  document.getElementById('wrapper')
-);
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={Wrapper}>
+      <IndexRoute component={LoginView} />
+    </Route>
+  </Router>
+), document.getElementById('wrapper'));
