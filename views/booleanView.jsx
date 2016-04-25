27584c5087bba2c87efdea4c9d3d607516a52943
Matthew Pace <pace4@purdue.edu>
2016-04-25 00:45:19 -0400

    Added server communication and modal to buy button

--- views/booleanView.jsx
+++ views/booleanView.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import $ from 'jquery';
-import { Grid, Row, Col, PageHeader, Alert, Panel, Button } from 'react-bootstrap';
+import { Grid, Row, Col, PageHeader, Alert, Modal, Panel, Button } from 'react-bootstrap';
 
 class NotFound extends React.Component {
   render() {
@@ -20,6 +20,39 @@ class NotFound extends React.Component {
 }
 
 class StatRow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.buyStatFromServer = this.buyStatFromServer.bind(this);
+
+    this.state = {
+      has: false
+    };
+  }
+
+  componentDidMount() {
+    this.setState({has: this.props.stat.has});
+  }
+
+  buyStatFromServer() {
+    $.ajax({
+        url: '/api/boolean/' + this.props.id + '/buyStat',
+        dataType: 'json',
+        cache: false,
+        data: {stat: this.props.stat.name},
+        success: function(data) {
+          if (data.bought) {
+            this.setState({has: true});
+          } else {
+            this.props.showModal();
+          }
+        }.bind(this),
+        error: function(xhr, status, err) {
+          this.props.showModal();
+          // console.error('/api/boolean/' + this.props.id + '/buyStat', status, err.toString());
+        }.bind(this)
+    });
+  }
+
   render() {
     return (
       <Row className="rowItem">
@@ -27,7 +60,7 @@ class StatRow extends React.Component {
           <p>Is {this.props.stat.has ? "" : "not"} {this.props.stat.name}</p>
         </Col>
         <Col lg={4}>
-          <Button bsStyle="success" disabled={this.props.stat.has || !this.props.alive}>Buy</Button>
+          <Button bsStyle="success" disabled={this.state.has || !this.props.alive} onClick={this.buyStatFromServer}>Buy</Button>
         </Col>
       </Row>
     );
@@ -37,14 +70,22 @@ class StatRow extends React.Component {
 class BooleanView extends React.Component {
   constructor(props) {
     super(props);
+    this.getInfoFromServer = this.getInfoFromServer.bind(this);
+    this.showModal = this.showModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
 
     this.state = {
       name: "",
       alive: true,
       streaks: {},
       stats: [],
-      error: false
+      error: false,
+      showModal: false
     };
+  }
+
+  componentDidMount() {
+    this.getInfoFromServer();
   }
 
   getInfoFromServer() {
@@ -66,8 +107,12 @@ class BooleanView extends React.Component {
     });
   }
 
-  componentDidMount() {
-    this.getInfoFromServer();
+  showModal() {
+    this.setState({showModal: true});
+  }
+
+  closeModal() {
+    this.setState({showModal: false});
   }
 
   render() {
@@ -75,36 +120,49 @@ class BooleanView extends React.Component {
       return <NotFound />;
     }
 
-    var alive = this.state.alive;
+    var context = this;
 
     var stats = this.state.stats.map(function(stat) {
       return <StatRow
         key={stat.name}
         stat={stat}
-        alive={alive} />;
+        alive={context.state.alive}
+        id={context.props.params.id}
+        showModal={context.showModal} />;
     });
 
     return(
-      <Grid>
-        <Col>
-          <PageHeader>{this.state.name}</PageHeader>
+      <div>
+        <Grid>
+          <Col>
+            <PageHeader>{this.state.name}</PageHeader>
 
-          <Row>
-            <Col lg={8}>
-              <Panel>
-                <p>Wins: {this.state.streaks.wins}</p>
-                <p>Losses: {this.state.streaks.losses}</p>
-                <p>Current Win Streak: {this.state.streaks.streak}</p>
-              </Panel>
-            </Col>
-            <Col lg={4}>
-              <Panel>
-                {stats}
-              </Panel>
-            </Col>
-          </Row>
-        </Col>
-      </Grid>
+            <Row>
+              <Col lg={8}>
+                <Panel>
+                  <p>Wins: {this.state.streaks.wins}</p>
+                  <p>Losses: {this.state.streaks.losses}</p>
+                  <p>Current Win Streak: {this.state.streaks.streak}</p>
+                </Panel>
+              </Col>
+              <Col lg={4}>
+                <Panel>
+                  {stats}
+                </Panel>
+              </Col>
+            </Row>
+          </Col>
+        </Grid>
+
+        <Modal show={this.state.showModal} onHide={this.closeModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>Insufficient Tautologies</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>You lack the tautologies to add a new stat to your Boolean.</p>
+          </Modal.Body>
+        </Modal>
+      </div>
     );
   }
 }
