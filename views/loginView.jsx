bcbefd968f54f7900ac7af8226828afce9fa69bd
Matthew Pace <pace4@purdue.edu>
2016-04-24 20:20:57 -0400

    Beef up loginView

--- views/loginView.jsx
+++ views/loginView.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Grid, Row, Col, PageHeader } from 'react-bootstrap';
 
 class LoginView extends React.Component {
   render() {
     return (
-      <div>
-        Hello, World!
-      </div>
+      <Grid>
+        <Row>
+          <Col>
+            <PageHeader>Welcome to Fighting Booleans!</PageHeader>
+            <p>Please sign in with Facebook to start fighting your Booleans.</p>
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 }
