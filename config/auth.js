57be8dcfaaa3dff002493aa9b4882dd77eb1576c
Matthew Pace <pace4@purdue.edu>
2016-04-26 11:32:27 -0400

    ports

--- config/auth.js
+++ config/auth.js
@@ -4,6 +4,6 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '265118990493490',
         'clientSecret'  : '21acbc29fac79d69188785a6f36b3b6c',
-        'callbackURL'   : 'http://localhost:8000/auth/facebook/callback'
+        'callbackURL'   : 'https://fightingbooleans.mybluemix.net/auth/facebook/callback'
     },
 };
